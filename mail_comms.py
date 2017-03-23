01b16b12d1e779fb7e9ff77c8fb820d546f7826a
Sean Newman <sxn6296@rit.edu>
2017-03-22 20:58:16 -0400

    Added mail_comms module
    Added the mail_comms module, which contains the send_print, recv_print,
    and send_recv functions.  With the addition of optional arguments, these
    are the same functions from the mail_client module that will now work
    with either the client or the server.

---
+++ mail_comms.py
@@ -0,0 +1,21 @@
+# Sean Newman
+import socket
+
+def send_print(sock, data, server=False):
+    sock.sendall(data)
+    if server:
+        print 'SERVER: ' + data
+    else:
+        print 'CLIENT: ' + data
+
+def recv_print(sock, server=False):
+    data = sock.recv(1024)
+    if server:
+        print 'CLIENT: ' + data
+    else:
+        print 'SERVER: ' + data
+
+dev send_recv(sock, data, server=False):
+    send_print(sock, data, server)
+    recv_print(sock, server)
+
