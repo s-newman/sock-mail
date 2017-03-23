e592e1f6c7ecbd482b0f4feb838bc3f6ac5b1c16
Sean Newman <sxn6296@rit.edu>
2017-03-22 21:36:39 -0400

    Fixed argument order
    Fixed the order of arguments in the extended function calls in the
    mail_server module.

--- mail_server.py
+++ mail_server.py
@@ -20,32 +20,32 @@ while True:
     conn, addr = sock.accept()
     print 'connected to ', addr
 
-    send_print(server=True, conn, '200 mail.rit.edu ESMTP Postfix')
+    send_print(conn, '200 mail.rit.edu ESMTP Postfix', server=True)
     connected = True
 
     # handle each command
     while connected:
-        data = recv_print(server=True, conn)
+        data = recv_print(conn, server=True)
         
         if data[0] == 'HELO':
             conn.sendall()
-            send_print(server=True, conn,
-                    '250 Hello ' + data[1] + ', I am glad to meet you')
+            send_print(conn, '250 Hello ' + data[1] +
+                    ', I am glad to meet you', server=True)
 
         elif data[0] == 'MAIL':
             addr_start = data[1].find('<')
             addr_end = data[1].find('>')
             from_addr = data[1][addr_start + 1 : addr_end]
-            send_print(server=True, conn, '250 Ok')
+            send_print(conn, '250 Ok', server=True)
 
         elif data[0] == 'RCPT':
             addr_start = data[1].find('<')
             addr_end = data[1].find('>')
             from_addr = data[1][addr_start + 1 : addr_end]
-            send_print(server=True, conn, '250 Ok')
+            send_print(conn, '250 Ok', server=True)
 
         elif data[0] == 'DATA':
-            send_print(server=True, conn, '354 End data with <CRLF>.<CRLF>')
+            send_print(conn, '354 End data with <CRLF>.<CRLF>', server=True)
             record = True
             while record:
                 entry = conn.recv(1024)
@@ -53,13 +53,13 @@ while True:
                 if entry == '.':
                     record = False
                     QUEUE += 1
-                    send_print(server=True, conn,
-                            '250 Ok: queued as ' + str(QUEUE))
+                    send_print(conn, '250 Ok: queued as ' + str(QUEUE),
+                            server=True)
                 else:
                     content += entry
             
         elif data[0] == 'QUIT':
-            send_print(server=True, conn, '221 Bye')
+            send_print(conn, '221 Bye', server=True)
             conn.close()
             connected = False
 
